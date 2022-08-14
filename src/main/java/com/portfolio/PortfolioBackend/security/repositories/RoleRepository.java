169c5c58ce34f21d37456050342128a0db5936ce
Eze Muñoz <eze.munoz@mi.unc.edu.ar>
2022-08-14 13:40:19 -0300

    changed pool size

--- src/main/java/com/portfolio/PortfolioBackend/security/repositories/RoleRepository.java
+++ src/main/java/com/portfolio/PortfolioBackend/security/repositories/RoleRepository.java
@@ -3,10 +3,12 @@ package com.portfolio.PortfolioBackend.security.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.portfolio.PortfolioBackend.security.entities.Role;
 import com.portfolio.PortfolioBackend.security.enums.RoleList;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findByRoleName(RoleList roleName);
 }
