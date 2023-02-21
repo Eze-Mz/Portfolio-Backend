b93fee392d10a3705ffc4f9585887b3b377a961d
Eze Muñoz <eze.munoz@mi.unc.edu.ar>
2023-02-21 18:45:19 -0300

    trying env variables

--- src/main/java/com/portfolio/PortfolioBackend/security/jwt/JwtProvider.java
+++ src/main/java/com/portfolio/PortfolioBackend/security/jwt/JwtProvider.java
@@ -29,6 +29,7 @@ public class JwtProvider {
     private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
     // clave para verificar el token
+    private String secret2 = System.getProperties().getProperty("jwt.secret");
     @Value("${jwt.secret}")
     private String secret;
 
@@ -38,6 +39,7 @@ public class JwtProvider {
 
     // función que crea el token
     public String generateToken(Authentication auth) {
+        logger.info(secret2);
         MainUser mainUser = (MainUser) auth.getPrincipal();
         SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
 
